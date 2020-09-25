4041a755d9558e795117a4f4354827a34281c222
ElijahWargo <Elijah.wargo@patelhs.org>
2020-09-25 19:21:25 +0000

    I added an age + 1 and switched "I am" to "I am still". If you would like me to go into more detail I can but this currently is the best way that I can explain it.

--- Main.java
+++ Main.java
@@ -1,5 +1,16 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    int age= 16;
+    int ageAfterBirthday= age+1;
+    int myBirthday= 422;
+    int todaysDate= 921;
+    //Inserted variable for age, my birthday, and todays date.
+    if (myBirthday == todaysDate)
+    {
+      System.out.println ("If my birthday is " + myBirthday + " and todays date is " + todaysDate + " I am " + ageAfterBirthday + " years old.");
+      //Wrote line of code that tells age if birthday is today.
+    }else{
+      System.out.println ("Since my birthday is " + myBirthday + " and today is " + todaysDate + " I am still " + age + " years old.");
+    }
   }
 }
